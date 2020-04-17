03ea07247bf19b5ffc00627279b24f1565728029
Jérémy Veillette <1830942@cshawi.ca>
2020-04-17 15:07:42 -0400

    5.5

--- Inventaire/ViewModels/MainViewModel.cs
+++ Inventaire/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using BillingManagement.UI.ViewModels.Commands;
+﻿using BillingManagement.Models;
+using BillingManagement.UI.ViewModels.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,11 +23,12 @@ namespace BillingManagement.UI.ViewModels
 		InvoiceViewModel invoiceViewModel;
 
 		public ChangeViewCommand ChangeViewCommand { get; set; }
+		public RelayCommand NewCustomerCommand { get; private set; }
 
 		public MainViewModel()
 		{
 			ChangeViewCommand = new ChangeViewCommand(ChangeView);
-
+			NewCustomerCommand = new RelayCommand(NewCustomer);
 			customerViewModel = new CustomerViewModel();
 			invoiceViewModel = new InvoiceViewModel(customerViewModel.Customers);
 
@@ -46,6 +48,12 @@ namespace BillingManagement.UI.ViewModels
 					break;
 			}
 		}
-
+		private void NewCustomer(object c)
+		{
+			Customer customer = new Customer();
+			customerViewModel.Customers.Add(customer);
+			customerViewModel.SelectedCustomer = customer;
+			VM = customerViewModel;
+		}
 	}
 }
